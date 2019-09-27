2d3963086450f834dbfaf98164504e195f17b54b
Administrator <Administrator@koitt03a-PC>
2019-09-27 15:19:38 +0900

    commit test

--- src/study01/test03/HowOldRU.java
+++ src/study01/test03/HowOldRU.java
@@ -9,6 +9,24 @@ public class HowOldRU
 		Scanner scan = new Scanner(System.in);
 		System.out.println("how old are you?");
 		String age = scan.nextLine();
-		System.out.println("난" + age + "살이야");
+		//System.out.println("난" + age + "살이야");
+		int num = Integer.parseInt(age);
+		
+		if(num <= 10)
+		{
+			System.out.println(num + "살은 어린이");
+		}
+		else if(num <= 20)
+		{
+			System.out.println(num + "살은 청소년");
+		}
+		else if(num <= 40)
+		{
+			System.out.println(num + "살은 청년");
+		}
+		else
+		{
+			System.out.println(num + "살은 꼰대");
+		}
 	}
 }
